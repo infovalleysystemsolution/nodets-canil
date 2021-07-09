d343e6b7573be07537170bd8c120f7ce10cacd62
infovalleysystemsolution <infovalleysystemsolution.com>
2021-07-09 00:24:43 -0300

    atualizado em 08/07/2021

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,29 +2,28 @@ import { Request, Response } from 'express';
 
 export const home = ( req: Request, res: Response) =>{
 
-    // res.render('pages/page')
-
+     res.render('pages/page');
 
 }
 
 export const dogs = ( req: Request, res: Response) =>{
 
-    // res.render('pages/page')
-
+    res.send('dogs');
+    // res.render('pages/dogs');
 
 }
 
 export const cats = ( req: Request, res: Response) =>{
 
-    // res.render('pages/page')
-
+    res.send('cats');
+    // res.render('pages/cats');
 
 }
 
 export const fishes = ( req: Request, res: Response) =>{
 
-    // res.render('pages/page')
-
+    res.send('fishes');
+    // res.render('pages/fishes');
 
 }
 
